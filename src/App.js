c2320d2cdebccedefb1814e5fe792f1203bca8db
tim.varvikko <tim.varvikko59@gmail.com>
2021-02-24 10:45:50 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,37 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-    <section>
-<<<<<<< Updated upstream
-      <Switch>
-          <Route path ="/" component ={Landing} exact />
-          <Route path ="/menu/:id" component ={Menu} />
-          <Route path="/nav/:id" component ={Nav} />
-          <Route path="/About/:id" component={About} />
-          <Route path="/cart/:id" component={Cart} />
-          <Route path="/status/:id" component={Status} />
-        </Switch>
-=======
-     
+  return(
      <Switch>
       <Route path ="/" component ={Landing} exact />
       <Route path ="/menu/:id" component ={Menu} />
@@ -44,9 +14,7 @@ function App() {
 
 
      </Switch>
->>>>>>> Stashed changes
-    </section>
->>>>>>> Stashed changes
+
   );
 }
 
--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-<<<<<<< Updated upstream
-function landing(props) {
-    return (
-        <div>
-            
-=======
+
 function Landing() {
     return (
         <div>
             <Link to="/menu"><h1>Tryck Här</h1></Link>
->>>>>>> Stashed changes
         </div>
     );
 }
 
-export default landing;
+export default Landing;
