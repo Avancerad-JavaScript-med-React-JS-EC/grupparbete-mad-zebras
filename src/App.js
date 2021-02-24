92bed87006378fbb9cc2cf7700ec75256848a7c0
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2021-02-24 12:02:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
   return(
      <Switch>
         <Route path ="/" component ={Landing} exact />
-        <Route path ="/menu/:id" component ={Menu} />
-        <Route path="/nav/:id" component ={Nav} />
-        <Route path="/About/:id" component={About} />
-        <Route path="/cart/:id" component={Cart} />
-        <Route path="/status/:id" component={Status} />
+        <Route path ="/menu" component ={Menu} />
+        <Route path="/nav" component ={Nav} />
+        <Route path="/About" component={About} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/status" component={Status} />
     </Switch>
   );
 }
