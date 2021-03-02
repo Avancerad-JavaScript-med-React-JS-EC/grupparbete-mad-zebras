8e13c38748a31438796550cc2579038a995a19dd
Tara78 <70331037+Tara78@users.noreply.github.com>
2021-03-02 19:11:16 +0100

    CSS menu

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import CoffeeItem from './coffeeItem';
-import style from './menu.module.css'
+import style from './menu.module.css';
+import {useHistory} from 'react-router-dom'; 
+import imgUrlb from '../../assets/graphics/bag.svg';
+import imgURLn from '../../assets/graphics/navicon.svg'; 
+import imgUrlh from'../../assets/graphics/graphics-header.svg';
 
 
 
 function Menu() {
+    const history = useHistory(); 
     
     const [coffees, setCoffees] = useState([]);
 
@@ -14,16 +19,28 @@ function Menu() {
         .then (response=>{ setCoffees(response.menu) })
     }, [])
 
+
     
 
     return (
         <section  className={ style.section }>
-            <button></button>
-            <button></button>
+            <div  className={ style.header} > <img src={imgUrlh }allt='header' />
+            <div className={ style.nav}>
+            <div className={ style.bag}>
+            <img src={imgUrlb} onClick={()=>history.push('/cart')} />
+            </div>
+            <div className={style.nav } > 
+            <img src={imgURLn} allt ='navicon' onClick={()=>history.push('/nav')} />
+            </div>
+            </div>
+    
+            </div>
             <h1 className={ style.title }>Meny</h1>
             {coffees.map((coffee) => {
                  return  <CoffeeItem coffee={ coffee } key={ coffee.id }/> 
             })}
+
+            
         </section>
     )
 }
