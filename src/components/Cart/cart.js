889f409e53e5656aaee69e911b55c06bb996131e
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2021-03-03 21:34:17 +0100

    Update new style to Cart

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 import style from './cart.module.css'; 
 import arrowUp from '../../assets/graphics/arrow-up.svg'
 import arrowDown from '../../assets/graphics/arrow-down.svg'
 
 function Cart() {
+
+    const history = useHistory();
 
     const coffee = useSelector((state) => {return state})
     console.log(coffee)
@@ -26,7 +29,7 @@ function Cart() {
             <div className={style.infoContainer}>
 
                 <div className={style.objects}>
-                    <h2 className={style.menuTitle}>ObjectsTitle</h2>
+                    <h2 className={style.menuTitle}>ObjectsTitle <span>...................</span></h2>
                     <p className={style.menuPrice}>ObjectsPrice kr</p>
                 </div>
 
@@ -39,17 +42,17 @@ function Cart() {
 
             <div className={style.totalContainer}>
                 <div className={style.total}>
-                    <h1 className={ style.totalTitle }>Total ....................</h1>
-                    <p>inkl moms + drönarleverans</p>
+                    <h1 className={ style.totalTitle }>Total <span>....................................................</span></h1>
+                    <p className={style.moms}>inkl moms + drönarleverans</p>
                 </div>
 
                 <div className={style.priceContainer}>
-                    <h1>{ coffee.total_price }</h1>
+                    <h1 className={style.price}>{ coffee.total_price }</h1>
                 </div>
             </div>
 
             <div className={style.buttonContainer}>
-                <button className={ style.takeMyMoney }>Take my money!</button>
+                <button className={ style.takeMyMoney } onClick={ () => history.push(`/status`) }>Take my money!</button>
             </div>
 
         </section>
