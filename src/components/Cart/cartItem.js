ba16ea90cbb50c1382352367f9c7df7543a3e619
tim.varvikko <tim.varvikko59@gmail.com>
2021-03-05 14:48:04 +0100

    update Tim

--- src/components/Cart/cartItem.js
+++ src/components/Cart/cartItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import style from './cart.module.css';
 import arrowUp from '../../assets/graphics/arrow-up.svg'
 import arrowDown from '../../assets/graphics/arrow-down.svg'
 import { decreaseCoffee,increaseCoffee } from '../../actions/action'
 
+function useTotalCoffeePrice(id){
+    const {price, quantity} = useSelector(state => state.coffees.find((coffee) => coffee.id === id))
+    return price*quantity
+}
 
 function CartItem({ coffee }){
 
+    const totalPrice = useTotalCoffeePrice(coffee.id)
     const dispatch = useDispatch()
 
     function handeClickDecrease(){
@@ -21,7 +27,7 @@ function CartItem({ coffee }){
         <section>
 
             <h2 className={style.menuTitle}>{ coffee.title}</h2>
-            <p className={style.menuPrice}>{ coffee.price } kr</p>
+            <p className={style.menuPrice}>{ totalPrice } kr</p>
 
             <div className={style.arrowsContainer}>
                 <img src={arrowUp} alt='arrowUp' className={style.arrows} onClick={ handeClickIncrease }/>
